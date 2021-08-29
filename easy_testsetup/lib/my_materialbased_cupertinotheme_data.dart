cf79be56197740bfa17e2e4e2939189b51a5e94b
Fred Grott <fred.grott@gmail.com>
2021-08-29 11:33:49 -0500

    first pass at easy test setup
    first pass at easy test setup

---
+++ easy_testsetup/lib/my_materialbased_cupertinotheme_data.dart
@@ -0,0 +1,14 @@
+// Copyright 2021 Fredrick Allan Grott. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+import 'package:easy_testsetup/my_material_theme_data.dart';
+import 'package:flutter/material.dart';
+
+/// myMaterialbasedCupertinoThemeData
+/// @author Fredrick Allan Grott
+MaterialBasedCupertinoThemeData myMaterialBasedCupertinoThemeData = MaterialBasedCupertinoThemeData(
+  materialTheme: lightTheme,
+  
+  );
